33cdc0b3e9badd1f3d9e386c715831c25860541b
glamtap202-create <glamtap202@gmail.com>
2026-09-18 18:41:51 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,21 +1,21 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 
-import Hero from "./components/Hero";
-import ServiceCarousel from "./components/ServiceCarousel";
-import Categories from "./components/Categories";
-import Offers from "./components/Offers";
-import WhyChooseUs from "./components/WhyChooseUs";
-import Testimonials from "./components/Testimonials";
-// import Claimservices from "./components/Claimservices";
-import Download from "./components/Download";
-import Professional from "./components/Professional";
-import CelebritiesLoveUs from "./components/CelebritiesLoveUs";
-import BestSellingPackages from "./components/Bestsellingpackages";
-import FacialsGrid from "./components/Facialsgrid";
-import OmgDeals from "./components/Omgdeals";
-import BeBold from "./components/Bebold";
-import KoreanGlow from "./components/Koreanglow";
+// import Hero from "./components/Hero";
+// import ServiceCarousel from "./components/ServiceCarousel";
+// import Categories from "./components/Categories";
+// import Offers from "./components/Offers";
+// import WhyChooseUs from "./components/WhyChooseUs";
+// import Testimonials from "./components/Testimonials";
+// // import Claimservices from "./components/Claimservices";
+// import Download from "./components/Download";
+// import Professional from "./components/Professional";
+// import CelebritiesLoveUs from "./components/CelebritiesLoveUs";
+// import BestSellingPackages from "./components/Bestsellingpackages";
+// import FacialsGrid from "./components/Facialsgrid";
+// import OmgDeals from "./components/Omgdeals";
+// import BeBold from "./components/Bebold";
+// import KoreanGlow from "./components/Koreanglow";
 
 function Home() {
   return (
